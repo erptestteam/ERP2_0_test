f374c3cf9e087e6ebeb4b764ebd750ed4214bed4
cimo <chuzhen89@gmail.com>
2014-03-27 14:00:42 +0800

    feature update: change back time format.

--- ERP2_0_test/serializers.py
+++ ERP2_0_test/serializers.py
@@ -1,8 +1,8 @@
 # coding=utf-8
-import time
+# import time
 from tastypie.serializers import Serializer
 
 class TimeFormatSerializer(Serializer): 
     def format_datetime(self, data):
-        # return data.strftime("%Y-%m-%d %X %Z")
-        return time.mktime(data.timetuple())
+        return data.strftime("%Y-%m-%d %X %Z")
+        # return time.mktime(data.timetuple())

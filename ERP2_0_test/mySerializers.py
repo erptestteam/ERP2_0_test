38dfee074572b5c405dd0cd8bac9273621b2e6ff
cimo <chuzhen89@gmail.com>
2014-04-06 22:32:25 +0800

    bug fix: date format serializer

--- ERP2_0_test/mySerializers.py
+++ ERP2_0_test/mySerializers.py
@@ -6,3 +6,6 @@ class TimeFormatSerializer(Serializer):
     def format_datetime(self, data):
         return data.strftime("%Y-%m-%d %X %Z")
         # return time.mktime(data.timetuple())
+    
+    def format_date(self, data):
+        return data.strftime("%Y-%m-%d")
